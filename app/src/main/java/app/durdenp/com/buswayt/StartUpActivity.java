dbba7baf82f27647655b4c47ee53516ecd645faf
DurdenP <dpatti@auctacognitio.net>
2015-05-30 12:59:25 +0200

    Draft Layout

--- app/src/main/java/app/durdenp/com/buswayt/StartUpActivity.java
+++ app/src/main/java/app/durdenp/com/buswayt/StartUpActivity.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class StartUpActivity extends ActionBarActivity {
@@ -19,6 +21,9 @@ public class StartUpActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(tag, "Chiamo onCreate");
+
+
+
         setContentView(R.layout.activity_main);
         final Handler handler = new Handler();
         intent = new Intent(this, ChooseModeActivity.class);
@@ -73,44 +78,4 @@ public class StartUpActivity extends ActionBarActivity {
 //    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
